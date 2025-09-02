6fd104f9fb3adf230e812a3f2af45fea23962838
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-09-02 15:20:48 +0900

    Update PMTHit.hh

--- include/PMTHit.hh
+++ include/PMTHit.hh
@@ -1,17 +1,23 @@
-// PMTHit.hh (리팩토링 제안)
 #ifndef PMTHit_h
 #define PMTHit_h 1
+
 #include "G4VHit.hh"
 #include "G4THitsCollection.hh"
 #include "G4Allocator.hh"
 
+/**
+ * @class PMTHit
+ * @brief PMT의 광음극에 도달한 광학 광자(hit) 정보를 저장하는 데이터 클래스입니다.
+ */
 class PMTHit : public G4VHit
 {
 public:
   PMTHit();
   virtual ~PMTHit();
 
-  // --- [리팩토링] 세그먼트 ID 저장을 위한 멤버 추가 ---
+  inline void* operator new(size_t);
+  inline void  operator delete(void*);
+
   void SetSegmentID(G4int id) { fSegmentID = id; }
   G4int GetSegmentID() const { return fSegmentID; }
 
@@ -26,5 +32,19 @@ private:
   G4int fPMTID;     // 세그먼트 내 PMT의 번호 (0 또는 1)
   G4double fTime;   // 광자 검출 시간
 };
+
 typedef G4THitsCollection<PMTHit> PMTHitsCollection;
+extern G4ThreadLocal G4Allocator<PMTHit>* PMTHitAllocator;
+
+inline void* PMTHit::operator new(size_t)
+{
+  if (!PMTHitAllocator) PMTHitAllocator = new G4Allocator<PMTHit>;
+  return (void*)PMTHitAllocator->MallocSingle();
+}
+
+inline void PMTHit::operator delete(void* aHit)
+{
+  PMTHitAllocator->FreeSingle((PMTHit*)aHit);
+}
+
 #endif
