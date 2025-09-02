71869499971963f11ae58cf1661df72345d3f427
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-09-03 01:52:25 +0900

    Update MyShieldingPhysList.cc

--- src/MyShieldingPhysList.cc
+++ src/MyShieldingPhysList.cc
@@ -1,5 +1,5 @@
 #include "MyShieldingPhysList.hh"
-#include "MyHadronPhysics.hh" // 우리가 만든 커스텀 강입자 물리
+#include "MyHadronPhysics.hh" // 사용자 정의 강입자 물리
 
 // Geant4 표준 물리 리스트 부품들
 #include "G4DecayPhysics.hh"
@@ -14,10 +14,10 @@ MyShieldingPhysList::MyShieldingPhysList(G4int verbose)
     SetVerboseLevel(verbose);
     defaultCutValue = 0.7 * mm;
 
-    // 1. 붕괴 물리 (Decay Physics)
+    // 1. (소립자) 붕괴 물리 (Decay Physics)
     RegisterPhysics(new G4DecayPhysics(verbose));
 
-    // 2. 방사성 붕괴 물리 (Radioactive Decay Physics)
+    // 2. 방사성 (핵) 붕괴 물리 (Radioactive Decay Physics)
     RegisterPhysics(new G4RadioactiveDecayPhysics(verbose));
 
     // 3. 전자기 물리 (Electromagnetic Physics) - Option 4는 정밀도가 높은 모델
@@ -36,12 +36,12 @@ MyShieldingPhysList::~MyShieldingPhysList()
 
 void MyShieldingPhysList::ConstructParticle()
 {
-    // 각 물리 부품들이 필요로 하는 입자들을 생성합니다.
+    // 각 물리 부분들이 필요로 하는 입자들을 생성합니다.
     G4VModularPhysicsList::ConstructParticle();
 }
 
 void MyShieldingPhysList::ConstructProcess()
 {
-    // 각 물리 부품들이 입자에 맞는 물리 프로세스를 등록합니다.
+    // 각 물리 부분들이 입자에 맞는 물리 프로세스를 등록합니다.
     G4VModularPhysicsList::ConstructProcess();
 }
