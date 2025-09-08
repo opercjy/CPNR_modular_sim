6838bf89ac347c358726974874f565a9cfd5bad5
Jiyoung CHOI <158591410+opercjy@users.noreply.github.com>
2025-09-09 02:38:26 +0900

    Delete src directory

--- src/SteppingAction.cc
+++
@@ -1,8 +0,0 @@
-#include "SteppingAction.hh"
-#include "G4Step.hh"
-
-SteppingAction::SteppingAction() : G4UserSteppingAction() {}
-
-SteppingAction::~SteppingAction() {}
-
-void SteppingAction::UserSteppingAction(const G4Step* /*step*/) {}
